c77007756c5d67f5073ab758381cfe356174add1
Darkweak <sylvain@les-tilleuls.coop>
2020-02-10 07:02:53 +0100

    Fix

--- api/src/Subscriber/JWTGenerationSubscriber.php
+++ api/src/Subscriber/JWTGenerationSubscriber.php
@@ -11,14 +11,16 @@ class JWTGenerationSubscriber
 	public function onJWTCreated(JWTCreatedEvent $event)
 	{
 		$user = $event->getUser();
+        $payload = $event->getData();
+        $payload['exp'] = time() + (60 * 60 * 24 * 30 * 6); // six months token lifetime
+        $payload['user'] = sprintf('/users/%s', $user->getId());
+        $payload['firstname'] = $user->getFirstname();
+        $payload['lastname'] = $user->getLastname();
 
-		if ($user->getToken()) {
+		/*if ($user->getToken()) {
 			$payload = null;
 		} else {
-			$payload = $event->getData();
-			$payload['exp'] = time() + (60 * 60 * 24 * 30 * 6); // six months token lifetime
-			$payload['user'] = sprintf('/users/%s', $user->getId());
-		}
+		}*/
 
 		$event->setData($payload);
 	}
